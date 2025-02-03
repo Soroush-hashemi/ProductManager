dcc9304b336301af74414768031f15bf64213ff3
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-03 16:31:39 +0330

    ProductController

--- ProductManager/Controllers/ProductController.cs
+++ ProductManager/Controllers/ProductController.cs
@@ -1,10 +1,80 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductManager.Models;
 
 namespace ProductManager.Controllers;
+[Route("/Product")]
 public class ProductController : Controller
 {
+    private readonly ProductDbContext _context;
+    public ProductController(ProductDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
         return View();
+    }
+
+    [Route("/GetList")]
+    public IActionResult GetList()
+    {
+        List<Product> products = _context.Products.ToList();
+        return View(products);
+    }
+
+
+    [Route("/Detail/{id}")]
+    public IActionResult Detail(int id)
+    {
+        Product product = GetProduct(id);
+        return View(product);
+    }
+
+    [HttpGet("/Add")]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost("/Add")]
+    public IActionResult Add(Product product)
+    {
+        _context.Products.Add(product);
+        _context.SaveChanges();
+
+        return Redirect("../");
+    }
+
+    [HttpGet("/Update/{id}")]
+    public IActionResult Update(int id)
+    {
+        Product product = GetProduct(id);
+        return View(product);
+    }
+
+    [HttpPut("/Update")]
+    public IActionResult Update(Product product)
+    {
+        _context.Products.Update(product);
+        _context.SaveChanges();
+        return Redirect("../");
+    }
+
+    [Route("/Remove/{id}")]
+    public IActionResult Remove(int id)
+    {
+        Product product = GetProduct(id);
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+        return Redirect("../");
+    }
+
+    private Product GetProduct(int id)
+    {
+        Product? product = _context.Products
+            .FirstOrDefault(p => p.Id == id);
+
+        return product;
     }
 }
