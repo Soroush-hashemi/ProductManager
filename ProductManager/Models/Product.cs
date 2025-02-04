396fd4a704b1f4ba9e701decc0020d60db9c8e60
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-04 12:14:01 +0330

    add constructor to model

--- ProductManager/Models/Product.cs
+++ ProductManager/Models/Product.cs
@@ -3,6 +3,12 @@ using System.ComponentModel.DataAnnotations;
 namespace ProductManager.Models;
 public class Product
 {
+    public Product(string name, int price)
+    {
+        Name = name;
+        Price = price;
+    }
+
     [Key]
     public long Id { get; set; }
     public string Name { get; set; } = null!;
